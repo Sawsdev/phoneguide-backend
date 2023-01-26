7dedb2f2b2feccabda4e8e7badbbf89086ac4fb2
Sergio Salazar <sawsdevx8@gmail.com>
2023-01-26 09:54:41 -0500

    Add Sentry error handler

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { dbConnService } = require('./src/mongo')
 dbConnService.dbConnect()
 const express = require('express')
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 const morgan = require('morgan')
 const cors = require('cors')
 const { PersonsApi } = require('./src/persons')
@@ -10,6 +12,22 @@ const notFound = require('./src/middlewares/notFound')
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+Sentry.init({
+  dsn: 'https://571fb18215ed41c29ed373cf51b393ac@o4504560793092096.ingest.sentry.io/4504560820092928',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app })
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
+
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
@@ -23,9 +41,21 @@ app.use(morgan(function (tokens, req, res) {
     tokens.body(req, res)
   ].join(' ')
 }))
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+/**
+ * Controllers
+ */
 PersonsApi(app)
 InfoApi(app)
 app.use(notFound)
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler())
+
 app.use(errorMiddleware)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
